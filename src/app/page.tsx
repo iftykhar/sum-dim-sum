11a38dbd41a1c46fff92ad3800437e31d92a3a6a
iftykhar alam <s.m.ifty49@gmail.com>
2025-08-01 03:18:55 +0600

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import Topsection from "@/components/landing/Topsection";
+import Hero from "@/components/landing/Hero";
+
 export default function Home() {
   return (
     <div className="">
       <Topsection></Topsection>
+      <Hero></Hero>
     </div>
   );
 }
