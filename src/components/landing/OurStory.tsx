d78bf458e541011acebfdbca3ee7e6fe3e1b41a8
iftykhar alam <s.m.ifty49@gmail.com>
2025-08-01 15:51:11 +0600

    our story section done

--- src/components/landing/OurStory.tsx
+++ src/components/landing/OurStory.tsx
@@ -4,7 +4,7 @@ import { PiCallBellLight } from "react-icons/pi";
 import { IoRibbonOutline } from "react-icons/io5";
 import { BiCalendarStar } from "react-icons/bi";
 import { FaArrowRightLong } from "react-icons/fa6";
-import {  Card,  CardAction,  CardContent,  CardDescription,  CardFooter,  CardHeader,  CardTitle,} from "@/components/ui/card"
+// import {  Card,  CardAction,  CardContent,  CardDescription,  CardFooter,  CardHeader,  CardTitle,} from "@/components/ui/card"
 
 
 const OurStory = () => {
@@ -14,28 +14,8 @@ const OurStory = () => {
         <div className="p-10">
           
           <div className="w-full flex gap-8">
-                {/* <div className="w-3/4">
-                
-                  <div className="bg-pink-50 p-8 rounded-lg"> 
-                      <h1 className="text-5xl font-bold mb-6" style={{ fontFamily: '"Cinzel Decorative", cursive' }}>
-                      OUR STORY
-                      </h1>
-                      <p className="text-gray-700 mb-6" style={{ fontFamily: '"Raleway", sans-serif' }}>
-                      At Sum Dim Sum, we are a local Singaporean brand that prepares dim sum dishes with wholehearted dedication and passion. Our chefs use traditional techniques to craft our dishes, ensuring that they are of the highest quality. Come and experience our fresh and flavorful cuisine.
-                      </p>
-                      <button className="bg-red-600 text-white px-6 py-2 rounded-md hover:bg-red-700 transition-colors">
-                      Learn More →
-                      </button>
-                  </div>
-
-                  <div className="relative w-[50%] h-auto p-4">
-                      <div className="relative">
-                        <img  src="/Rectangle401.png" alt="Sum Dim Sum storefront" className="w-full h-auto rounded-lg relative z-10"/>
-                      <div className="absolute inset-0 border-2 border-red-500 rounded-lg -translate-x-4 -translate-y-4 z-0" />
-                      </div>
-                  </div>
-                </div> */}
-                <div className="w-3/4 relative p-20"> {/* Added relative positioning to parent */}
+               
+                <div className="w-3/4 relative p-20"> 
                   <div className="bg-pink-50 p-14 rounded-lg"> 
                     <h1 className="text-5xl font-bold mb-6" style={{ fontFamily: '"Cinzel Decorative", cursive' }}>
                       OUR STORY
