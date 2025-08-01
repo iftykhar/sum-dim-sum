d17693deefb49f25001acb9a8d4afb898a270679
iftykhar alam <s.m.ifty49@gmail.com>
2025-08-01 20:07:24 +0600

    working on comment section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Topsection from "@/components/landing/Topsection";
 import Hero from "@/components/landing/Hero";
 import OurStory from "@/components/landing/OurStory";
 import Tradition from "@/components/landing/Tradition";
+import Info from "@/components/landing/Info";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero></Hero>
       <OurStory></OurStory>
       <Tradition></Tradition>
+      <Info></Info>
     </div>
   );
 }
